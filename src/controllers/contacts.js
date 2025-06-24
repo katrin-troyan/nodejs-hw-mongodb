4f9b0686d8d2d1ccc54085ae1ccbc2dc73ca97f2
Kateryna Troyan <ket.tro@gmail.com>
2025-06-24 16:48:58 +0300

    hw3

---
+++ src/controllers/contacts.js
@@ -0,0 +1,69 @@
+import createHttpError from 'http-errors';
+import { getAllContacts, getContactById } from '../services/contacts.js';
+import { createContact } from '../services/contacts.js';
+import { patchContact } from '../services/contacts.js';
+import { deleteContact } from '../services/contacts.js';
+
+export const getContactsController = async (req, res, next) => {
+    try {
+        const contacts = await getAllContacts();
+        res.json({
+            status: 200,
+            message: 'Successfully found contacts!',
+            data: contacts,
+        });
+    }
+    catch (error) {
+        next(error);
+    }
+};
+
+export const getContactByIdController = async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+        throw createHttpError(404, 'Contact not found');
+    }
+    res.json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+    });
+}
+
+export const createContactController = async (req, res) => {
+      const contact = await createContact(req.body);
+    res.status(201).json({
+        status: 201,
+        message: 'Successfully created contact!',
+        data: contact,
+    });
+};
+
+export const patchContactController = async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await patchContact(contactId, req.body);
+
+    if (!contact) {
+        next(createHttpError(404, 'Contact not found'));
+    return;
+    }
+    res.json({
+        status: 200,
+        message: `Successfully patched a contact!`,
+        data: contact,
+    });
+}
+
+export const deleteContactController = async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await deleteContact(contactId);
+
+    if (!contact) {
+        next(createHttpError(404, 'Contact not found'));
+        return;
+    }
+    res.status(204).send();
+    };
+
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,21 @@ export const getContactById = async (contactId) => {
     const contact = await Contact.findById(contactId);
     return contact;
   };
+
+export const createContact = async (playload) => {
+    const contact = await Contact.create(playload);
+    return contact
+  }
+
+export const patchContact = async (contactId, playload) => {
+    const contact = await Contact.findByIdAndUpdate(contactId, playload, {
+        new: true,
+        runValidators: true,
+    });
+    return contact;
+}
+
+export const deleteContact = async (contactId) => {
+    const contact = await Contact.findByIdAndDelete(contactId);
+    return contact;
+};
